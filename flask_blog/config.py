740f873686067bc0da55c87da34a32098c141505
Dev Kumar Pal <72178142+devkumar2313@users.noreply.github.com>
2025-10-02 13:37:25 +0530

    Update config.py

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -4,8 +4,11 @@ import secrets
 class Config:
     SECRET_KEY = secrets.token_hex()
     SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_DEFAULT_SENDER = os.environ.get('EMAIL_USER')
+    MAIL_MAX_EMAILS = None
+    MAIL_ASCII_ATTACHMENTS = False
